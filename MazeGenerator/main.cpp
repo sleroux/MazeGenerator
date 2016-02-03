cf00a2f0c594b172eb0ffb1455c102532be97b4e
Stephan Leroux <sleroux@mozilla.com>
2016-02-03 12:20:06 -0500

    Initial Commit

---
+++ MazeGenerator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MazeGenerator
+//
+//  Created by Steph Leroux on 2016-02-03.
+//  Copyright © 2016 Stephan Leroux. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
